28e61ce3d2327c986ddf644abbcde4601684ece7
Mohammed Dewidar <dewiidar@yahoo.com>
2019-10-18 22:50:53 +0200

    committing files

---
+++ src/someModule.js
@@ -0,0 +1,12 @@
+function display (message) {
+    console.log(message)
+}
+
+function log (message) {
+    console.log(message)
+}
+
+export default {
+    display: display,
+    log: log
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: [
+            './src/main.js', './src/main.scss'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            // webpack
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+
+        ]
+    },
+    plugins: []
+}
